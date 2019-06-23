9f13c4e61f6096120951c5357da661388effa580
Robin Bayer <robin.bayer@tequacreek.net>
2019-06-23 09:29:15 -0400

    Build proxy functions for Contact
    Built out AddContact, UpdateContact, DeleteContact local proxy functions

---
+++ APICallResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Overthink.SimpleContactsWSClientLibrary
+{
+    public class APICallResult
+    {
+        public int resultCode { get; set; }
+        public string successKeyValue { get; set; }
+        public List<string> errors { get; set; }
+
+    }
+}
---
+++ Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Overthink.SimpleContactsWSClientLibrary
+{
+    public class Contact
+    {
+        public string contactId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string cellPhone { get; set; }
+        public string homePhone { get; set; }
+        public string workPhone { get; set; }
+        public string emailAddress { get; set; }
+        public DateTime birthDate { get; set; }
+        public string locationAddress { get; set; }
+
+    }
+}
